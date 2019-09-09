040fa2984d71e901be43a8c6e1696a2d82103bdf
NaginiChen <naginichen@gmail.com>
2019-09-08 20:03:22 -0400

    New images, faq+about section, mobile polish

---
+++ assets_2020/js/faq.js
@@ -0,0 +1,30 @@
+$(function () {
+    $(".question").click(function () {
+        $(".question").css("background-color", "white", "color", "black");
+        $(this).css("background-color", "#808CFB", "color", "white");
+    });
+});
+$(function () {
+    $("#transportation").click(function () {
+        $("#answerBubble").empty();
+        $("#answerBubble").append("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat ");
+    });
+});
+$(function () {
+    $("#who").click(function () {
+        $("#answerBubble").empty();
+        $("#answerBubble").append("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat");
+    });
+});
+$(function () {
+    $("#packing").click(function () {
+        $("#answerBubble").empty();
+        $("#answerBubble").append("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat");
+    });
+});
+$(function () {
+    $("#codeOfConduct").click(function () {
+        $("#answerBubble").empty();
+        $("#answerBubble").append("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat");
+    });
+});

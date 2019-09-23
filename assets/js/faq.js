8bad04ec0e989c4ccad53eccb430935c0083b32e
NaginiChen <naginichen@gmail.com>
2019-09-23 04:58:43 -0400

    Small style changes
    faq answer bubble temporary fix

--- assets/js/faq.js
+++ assets/js/faq.js
@@ -7,24 +7,24 @@ $(function () {
 $(function () {
     $("#when").click(function () {
         $("#answerBubble").empty();
-        $("#answerBubble").append("The application will be open in early November!");
+        $("#answerBubble").append("<p>The application will be open in early November!</p>");
     });
 });
 $(function () {
     $("#who").click(function () {
         $("#answerBubble").empty();
-        $("#answerBubble").append("We welcome female, trans, and non-binary individuals of all skill levels who have a passion for learning more about technology and are 16 years or older.");
+        $("#answerBubble").append("<p>We welcome female, trans, and non-binary individuals of all skill levels who have a passion for learning more about technology and are 16 years or older.</p>");
     });
 });
 $(function () {
     $("#travel").click(function () {
         $("#answerBubble").empty();
-        $("#answerBubble").append("A limited number of travel scholarships will be provided. More info will be included in our application!");
+        $("#answerBubble").append("<p>A limited number of travel scholarships will be provided. More info will be included in our application!</p>");
     });
 });
 $(function () {
     $("#beginner").click(function () {
         $("#answerBubble").empty();
-        $("#answerBubble").append("We welcome beginners and experts alike! There will be plenty of workshops and mentors available to guide you throughout your 36-hour hacking journey, and you can also learn from other students!        ");
+        $("#answerBubble").append("<p>We welcome beginners and experts alike! There will be plenty of workshops and mentors available to guide you throughout your 36-hour hacking journey, and you can also learn from other students! </p>  ");
     });
 });

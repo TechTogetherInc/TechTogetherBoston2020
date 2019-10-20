62b40963b08ee5dba3dac7d581482afcce83fa21
NaginiChen <naginichen@gmail.com>
2019-10-20 01:09:31 -0400

    Sponsor Section Updated
    Sponsor section updated with more sponsors, FAQ anchor added

---
+++ assets/js/nav.js
@@ -0,0 +1,18 @@
+// Closes hamburger nav when FAQ is clicked on for mobile
+
+const faq = document.getElementById("faqButton");
+
+faq.addEventListener("click", closeNav);
+
+function closeNav() {
+    const toggle = document.getElementById("toggle");
+    const attribute = toggle.getAttribute("aria-expanded");
+    const navBar = document.getElementById("navbarNav");
+
+    if(attribute === "true"){
+        toggle.classList.add("collapsed");
+        navBar.classList.remove("show");
+    }
+
+}
+
